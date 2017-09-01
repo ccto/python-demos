ae7563c749a1904b144aee919ad7a8ba67ab16f2
凤舞九天 <outlook.com>
2017-09-01 17:55:38 +0800

    增加基础示例

--- 01-basic/octothorpe.py
+++ 01-basic/octothorpe.py
@@ -8,7 +8,17 @@
 
 print("Hello,world!")
 
-# 格式化输出
+# 格式化输出 
 print("Test %s is %d" % (1, 3))
 
 print("a","b",sep='')
+
+#格式化输出 1
+print("%s %d-%d" % ("hello",7,1))
+
+
+#格式化输出 2
+print("{0} {1}:{2}".format("hello",1,7))
+
+#格式化输出 3
+print("%.3f" % 1.23456)
